22dff3141758d93e331e8d43bb4283f2e7d5f35d
Kisung Kim <kskim80@gmail.com>
2018-07-05 06:27:22 +0900

    Add PG

--- node/node.go
+++ node/node.go
@@ -570,7 +570,8 @@ func (n *Node) OpenDatabase(name string, cache, handles int) (ethdb.Database, er
 	if n.config.DataDir == "" {
 		return ethdb.NewMemDatabase(), nil
 	}
-	return ethdb.NewLDBDatabase(n.config.resolvePath(name), cache, handles)
+	//return ethdb.NewLDBDatabase(n.config.resolvePath(name), cache, handles)
+	return ethdb.NewPGDatabase()
 }
 
 // ResolvePath returns the absolute path of a resource in the instance directory.
--- node/service.go
+++ node/service.go
@@ -43,7 +43,8 @@ func (ctx *ServiceContext) OpenDatabase(name string, cache int, handles int) (et
 	if ctx.config.DataDir == "" {
 		return ethdb.NewMemDatabase(), nil
 	}
-	db, err := ethdb.NewLDBDatabase(ctx.config.resolvePath(name), cache, handles)
+	//db, err := ethdb.NewLDBDatabase(ctx.config.resolvePath(name), cache, handles)
+	db, err := ethdb.NewPGDatabase()
 	if err != nil {
 		return nil, err
 	}
